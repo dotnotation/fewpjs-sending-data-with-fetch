4018557e5b3017c821ce69957a64b5830f692ef3
dotnotation <dotinmotionproductions@gmail.com>
2021-11-11 14:30:43 -0500

    commit tree 9c802a0dfe332a971e41fedfa40be0373ab5df1d

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify ({
+            name,
+            email
+        })
+    })
+    .then(resp => resp.json())
+    .then(object => {
+        const body = document.querySelector("body");
+        const ul = document.createElement("ul");
+        const li = document.createElement("li");
+        body.append(ul);
+        ul.append(li);
+        const userID = object.id;
+        li.innerHTML = userID;
+    })
+    .catch(error => document.body.innerHTML = error.message)
+}
